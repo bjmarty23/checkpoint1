8aa80a80dac0e61b6f26729bad11635556589121
Britni Marty <bjmarty23@gmail.com>
2018-03-12 14:34:37 -0500

    not sure its all messed up

--- client.js
+++ client.js
@@ -1,22 +1,36 @@
 /*jshint esversion: 6*/
-
+let count = 0;
 console.log('top');
+
 
 $(document).ready(readyNow);
 function readyNow(){
   console.log('js doc ready');
+  $('#contianer').empty();
   appendToDom();
+
 }
 
 function appendToDom(){
-$('#generate').on('click', function(){
+
+$('#generate').on('click', function (){
   console.log('on click');
 $('#container').css('background-color', 'red');
+onClick();
   });//end click
 }//end func
+function missed(){
+  outputElement = $('.buttons');
+  $('#generate').on('click');
+outputElement.append(<div id= "something"></div>)
+}
+function onClick(){
+count += 1;
+ console.log('count',count);
+ $('#container').append(count);
+}// cant get this dang thing to  stop being a string
 
-
-
-
-
+function thing(){
+$('#container').append('<button id="swap">Swap</button');
+}
 console.log('bottom');
