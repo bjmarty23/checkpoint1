15f946efaa3ad37afdaed9a5903335f46da64630
Britni Marty <bjmarty23@gmail.com>
2018-03-12 13:46:07 -0500

    button/background color

--- client.js
+++ client.js
@@ -5,10 +5,18 @@ console.log('top');
 $(document).ready(readyNow);
 function readyNow(){
   console.log('js doc ready');
+  appendToDom();
 }
 
-function click(){
-  $('generate').on('click');
-}
+function appendToDom(){
+$('#generate').on('click', function(){
+  console.log('on click');
+$('#container').css('background-color', 'red');
+  });//end click
+}//end func
+
+
+
+
 
 console.log('bottom');
