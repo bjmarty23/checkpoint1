6a693c6c2a90cc0ced978b351f3aeb24cc6038be
Britni Marty <bjmarty23@gmail.com>
2018-03-12 13:26:22 -0500

    generate button complete

--- client.js
+++ client.js
@@ -1,6 +1,14 @@
 /*jshint esversion: 6*/
 
-console.log(top);
+console.log('top');
 
+$(document).ready(readyNow);
+function readyNow(){
+  console.log('js doc ready');
+}
 
-console.log(bottom);
+function click(){
+  $('generate').on('click');
+}
+
+console.log('bottom');
